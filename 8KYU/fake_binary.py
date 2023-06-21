7e808aa319ba47f2e972ae25a42f0cd2beb440ce
Evgeny Elagin <anykeysky@gmail.com>
2023-06-21 16:13:56 +0300

    fake_bunary

--- 8KYU/fake_binary.py
+++ 8KYU/fake_binary.py
@@ -1,5 +1,13 @@
 def fake_bin(x):
+    s = ''
     for i in x:
-        print(x)
+        if i < '5':
+            s += '0'
+        if i == '5':
+            s += '1'
+        if i > '5':
+            s += '1'
+        print(s)
+    return s
 
-fake_bin('128383')
+fake_bin("123456789")
