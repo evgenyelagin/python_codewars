5db8de4050f1b5347d26138b6c8d77f7fb022eca
Evgeny Elagin <anykeysky@gmail.com>
2023-06-26 22:49:22 +0300

    calculate average 2

--- 8KYU/calculate_average.py
+++ 8KYU/calculate_average.py
@@ -3,3 +3,12 @@ def find_average(numbers):
         return 0
     else:
         return sum(numbers)/len(numbers)
+
+def find_average_2(numbers):
+    empty = []
+    if empty == numbers:
+        return 0
+    else:
+        s = sum(numbers)
+        avg = s/len(numbers)
+        return avg
