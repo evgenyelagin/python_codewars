ca32f7e05685f2e6af9a3a8e2515f8aa5cc76b64
Evgeny Elagin <anykeysky@gmail.com>
2023-11-01 17:00:18 +0300

    make_negative.py

---
+++ 8KYU/return_negative.py
@@ -0,0 +1,18 @@
+def make_negative( number ):
+    if number <= 0:
+        return number
+    else:
+        number *= (-1)
+        return number
+
+    def make_negative_2(number):
+        if number <= 0:
+            return number
+        else:
+            return -number
+
+    def make_negative_3(number):
+        if number > 0:
+            return -number
+        else:
+            return number
